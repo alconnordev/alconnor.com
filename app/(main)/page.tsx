6f37071ded658ee2968e79f1a20f12829ef5aa3c
alconnor <hi@alconnor.com>
2023-07-19 21:24:45 +0800

    feat: add profile avatar image and link to home page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,24 +2,35 @@ import Image from 'next/image'
 import { Card } from '~/components/Card'
 import { Profile } from '~/config/profile'
 import { ArrowRight, Facebook, Instagram, Twitter, Youtube } from 'lucide-react'
+import clsx from 'clsx'
+import Link from 'next/link'
+import avatarImage from '~/assets/profile/avatar.png'
 
 export default function BlogHomePage() {
   return (
     <div className="flex gap-3 text-white">
       <Card>
-        <div className="relative flex w-full items-center justify-around">
-          <Image
-            src={Profile.avatar}
-            className="inline-block rounded-full"
-            alt={'alconnor avatar'}
-            priority
-            width={80}
-            height={80}
-          />
+        <div className="relative flex w-full items-center justify-between">
+          <Link
+            href="/"
+            aria-label="Home"
+            className={clsx('pointer-events-auto')}
+          >
+            {/* https://nextjs.org/docs/app/building-your-application/optimizing/images#local-images */}
+            <Image
+              src={avatarImage}
+              alt={Profile.name}
+              sizes={'2.5rem'}
+              className={clsx(
+                'h-20 w-20 rounded-full bg-zinc-100 object-cover dark:bg-zinc-800'
+              )}
+              priority
+            />
+          </Link>
           <ArrowRight className="inline-block" width={48} height={48} />
         </div>
-        <p>A Portfolio Brimming with Innovation</p>
-        <div>Wei Chen • Freelance</div>
+        <p className="text-5xl">A Portfolio Brimming with Innovation</p>
+        <div className="mt-5 text-[#525252]">Wei Chen • Freelance</div>
       </Card>
 
       <div className="flex flex-col">
@@ -30,6 +41,7 @@ export default function BlogHomePage() {
           </div>
           <div>123</div>
         </Card>
+
         <div className="my-3 flex justify-around">
           <Card className="w-fit">
             <Twitter />
